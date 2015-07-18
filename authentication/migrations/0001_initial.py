329368ee6d8fb059d40b01ed5d572b16e7284b5d
tawanda <tminya@gmail.com>
2015-07-18 15:53:39 +0200

    yyyuy

---
+++ authentication/migrations/0001_initial.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import django.utils.timezone
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Account',
+            fields=[
+                ('id', models.AutoField(auto_created=True, serialize=False, primary_key=True, verbose_name='ID')),
+                ('password', models.CharField(verbose_name='password', max_length=128)),
+                ('last_login', models.DateTimeField(verbose_name='last login', default=django.utils.timezone.now)),
+                ('email', models.EmailField(unique=True, max_length=75)),
+                ('username', models.CharField(unique=True, max_length=40)),
+                ('first_name', models.CharField(blank=True, max_length=40)),
+                ('last_name', models.CharField(blank=True, max_length=40)),
+                ('tagline', models.CharField(blank=True, max_length=140)),
+                ('is_admin', models.BooleanField(default=False)),
+                ('created_at', models.DateTimeField(auto_now_add=True)),
+                ('updated_at', models.DateTimeField(auto_now=True)),
+            ],
+            options={
+                'abstract': False,
+            },
+            bases=(models.Model,),
+        ),
+    ]
